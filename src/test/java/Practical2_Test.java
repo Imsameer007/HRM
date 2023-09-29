8524ccdfe45bbef9b068cc82f701622c676ada0a
Imsameer007 <sameerkumar7931@gmail.com>
2023-09-29 20:26:15 +0530

    First commit-----

---
+++ src/test/java/Practical2_Test.java
@@ -0,0 +1,9 @@
+
+public class Practical2_Test {
+
+	public static void main(String[] args) {
+		System.out.println("hello everyone");
+
+	}
+
+}
---
+++ src/test/java/PracticalTest.java
@@ -0,0 +1,11 @@
+
+public class PracticalTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		System.out.println("Good Morning");
+		System.out.println("Hey guys created one project");
+
+	}
+
+}
